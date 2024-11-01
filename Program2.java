6789be0cfc17f96f97cdadc9f9b710aec5d5c58f
Anurag8853 <whyanurag8853@gmail.com>
2024-11-01 17:35:27 +0530

    Character_Data

---
+++ Program2.java
@@ -0,0 +1,25 @@
+public class Program2
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println('@');
+		System.out.println('d');
+		System.out.println('@'+10);
+		System.out.println('d'*3);
+		System.out.println('d'+'A');
+		System.out.println('d'-'D');
+		System.out.println('T'-'t');
+		System.out.println('d'+'A'+10);
+		System.out.println(2+2);
+		System.out.println('2'+2);
+		System.out.println('2'+'2');
+		System.out.println('2'>'A');
+		System.out.println('A'>'2');
+	
+	
+	}
+
+
+
+}
